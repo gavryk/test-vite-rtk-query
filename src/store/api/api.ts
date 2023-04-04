02973f75efee7db6c5a9e3969a56120dd4e81b1e
Gavryk <gvozdyk23@gmail.com>
2023-04-04 21:15:11 +0300

    change: process.env to import.meta.env

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -3,6 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
 	reducerPath: 'api',
 	tagTypes: ['Recipe'],
-	baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}/recipes` }),
+	baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.REACT_APP_BASE_URL}/recipes` }),
 	endpoints: (builder) => ({}),
 });
